f8fb8896bf53c1f8de56e009943d5b70d27cb0ea
jrmccann2 <jshmccann@gmail.com>
2019-11-18 15:27:55 -0700

    added gitignore

--- devmtn.js
+++ devmtn.js
@@ -1,1 +1,7 @@
-var testVar = "This is a test"
+/**
+ * In DevMtn folder, create a unit folder for each week of lecture (1-7)
+ * 
+ * In each unit folder, create a folder for each day (day-1, day-2, etc)
+ * 
+ * Try to use only the command line (bash)
+ */
